49916510a62769d1a83a68c5dda1f221e6baa0cd
Konstantin <loboda-nn@yandex.ru>
2022-07-09 15:10:45 +0300

    features/expandCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,110 +1,113 @@
-//массив готовых фото
-const initialCards = [
-  {
-    name: 'Питер"',
-    link: '../images/Zorro/photo_2022-06-13_18-00-43.jpg'
-  },
-  {
-    name: 'Бальные танцы"',
-    link: '../images/Zorro/photo_2022-06-13_18-02-19.jpg'
-  },
-  {
-    name: 'Птичка на ладони"',
-    link: '../images/Zorro/photo_2022-06-13_18-00-11.jpg'
-  },
-  {
-    name: 'Макро"',
-    link: '../images/Zorro/photo_2022-06-13_18-00-05.jpg'
-  },
-  {
-    name: 'Одуванчики"',
-    link: '../images/Zorro/photo_2022-06-13_17-59-20.jpg'
-  },
-  {
-    name: 'Пёсик"',
-    link: '../images/Zorro/photo_2022-06-13_17-59-45.jpg'
-  },
-  {
-    name: 'Ресторан "Борщ"',
-    link: 'https://downloader.disk.yandex.ru/preview/3a330064b6a4adaa5ae9f4331b271b30e18ad53df1c3c8caba367ddc809cd6e2/62c6a6d5/7TuFZINFR3stYN3AVxaWB_BYXmPS7R1a3ygpfR6uK3Sovy46qWwPRlssDBERTRq5uPRiQ-YtSlNmJNpSbYa_tA%3D%3D?uid=0&filename=AZH_3056%D0%B8%D1%8E%D0%BD%2022%202022.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1900x902'
-  },
-  {
-    name: 'Пирамида',
-    link: 'https://downloader.disk.yandex.ru/preview/1a3953ae565a5280e532793c9abb8effaaeef21625f45cc08efe05e0c0c63cec/62c6a6d5/05CdXPWfmfHBMTm4WUcjYAgeFpf2H2YL5BC_hZY1brcnzwMv7hEOtA0veJB_7ao0uSWnXD4PG0S9G0vJgen22Q%3D%3D?uid=0&filename=AZH_2946%D0%B8%D1%8E%D0%BD%2022%202022.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1900x902'
-  },
-  {
-    name: 'г.Пушкин. Мечеть',
-    link: 'https://downloader.disk.yandex.ru/preview/b713e28f24b9bf371f1e90a873e4aa512b2cbbb64abd71a73534ac8efbc24348/62c6a6d5/4-IssEo25Cn63W7EDOlXJuJYYrsxrQI55bHmtj-1q3MhNDps0B0cHeJfniG18X7JY3elRJWjKkrq21rolAgoEw%3D%3D?uid=0&filename=AZH_2948%D0%B8%D1%8E%D0%BD%2022%202022.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1900x902'
-  },
-  {
-    name: 'Старый мост',
-    link: 'https://downloader.disk.yandex.ru/preview/8f60941597c7bfc6f0e7bfc1f9056e7fc843fcc3692af31d30f4adbd49bb51dd/62c6a6d5/VynnzkwDQpaTAt-MZrJbp5tkW8Y6moZmVHCPVnNGt0gcQbbsror5Gp6axoVS8Y3nDSqs9N4vDMsohsX_taSkCw%3D%3D?uid=0&filename=AZH_2938%D0%B8%D1%8E%D0%BD%2022%202022.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1900x902'
-  },
-  {
-    name: 'Царские клумбы',
-    link: 'https://downloader.disk.yandex.ru/preview/b002697743cf06b1d08af651c271388f9077304a58fa30e4b42a8529a184800f/62c6a6d5/FPobNoM5y6UW9BwbIH7w5uHbjJvlHhOoOkLq3h4mNGUluYyIJAlfjRMHqVtIaZQt5JLoqAiIk_PIyba_dN5bUQ%3D%3D?uid=0&filename=AZH_3026%D0%B8%D1%8E%D0%BD%2022%202022.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1900x902'
-  },
-  {
-    name: 'Белка',
-    link: 'https://downloader.disk.yandex.ru/preview/524ab43df2d5c90231c367673e0cd1eaadf11e803ecf81a395e9b91c1651c9b3/62c6a6d5/8ntAir86_1vcrsXpPT-gaZVMKuW7jNEw6sQA381AQDxQDTASn2VeUaxAyIO1gn6wVp3jyAwRZaLJSrcFArpvIw%3D%3D?uid=0&filename=AZH_2986%D0%B8%D1%8E%D0%BD%2022%202022.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1900x902'
-  }
-];
+const selectors = {
+  popupEditProfile: '.popup_type_profile-edit',
+  popupAddCard: '.popup_type_add-card',
+  popupExpandPic: '.popup_type_expand-image',
+  inputUsername: '.form__input_type_username',
+  inputJob: '.form__input_type_job',
+  inputImgName: '.form__input_type_name',
+  inputImgLink: '.form__input_type_link',
+  userName: '.profile__title',
+  userJob: '.profile__description',
+  buttonEdit: '.profile__edit-button',
+  buttonAdd: '.profile__add-button',
+  buttonClose: '.popup__close',
+  buttonLike: '.element__like-button',
+  buttonDel: '.element__del-button',
+  form: '.popup__form',
+  cardsList: '.elements__list',
+  card: '.element',
+  cardImg: '.element__image',
+  cardName: '.element__title',
+  cardTemplate: '.element-tmp',
+  fullSizeImg: '.popup__image',
+  fullSizeImgCaption: '.popup__caption'
+};
 
 // ищем попап редактирования профиля
-const popupEditProfile = document.querySelector('.popup_type_profile-edit');
+const popupEditProfile = document.querySelector(selectors.popupEditProfile);
 // ищем кнопку для открытия попапа редактирования профиля
-const popupEditProfileButton = document.querySelector('.profile__edit-button');
+const popupEditProfileButton = document.querySelector(selectors.buttonEdit);
 // ищем кнопку закрытия попапа редактирования профиля
-const popupCloseButton = popupEditProfile.querySelector('.popup__close');
+const popupCloseButton = popupEditProfile.querySelector(selectors.buttonClose);
 // ищем форму попапа редактирования профиля
-const formEditProfileSubmit = popupEditProfile.querySelector('.popup__form');
+const formEditProfileSubmit = popupEditProfile.querySelector(selectors.form);
 // ищем инпуты (переменные) формы попапа редактирования профиля
-let nameInput = popupEditProfile.querySelector('.form__input_type_username');
-let jobInput = popupEditProfile.querySelector('.form__input_type_job');
+let nameInput = popupEditProfile.querySelector(selectors.inputUsername);
+let jobInput = popupEditProfile.querySelector(selectors.inputJob);
 // задаем переменные, в которые будут сохраняться данные из формы
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__description');
+let profileName = document.querySelector(selectors.userName);
+let profileJob = document.querySelector(selectors.userJob);
 
 // ищем попап добавления карточки
-const popupAddCard = document.querySelector('.popup_type_add-card');
+const popupAddCard = document.querySelector(selectors.popupAddCard);
 // ищем кнопку открытия попапа добавления карточки
-const popupAddCardButton = document.querySelector('.profile__add-button');
+const popupAddCardButton = document.querySelector(selectors.buttonAdd);
 // ищем кнопку для закрытия попапа добавления карточки
-const popupCloseAddCardButton = popupAddCard.querySelector('.popup__close');
+const popupCloseAddCardButton = popupAddCard.querySelector(selectors.buttonClose);
 // ищем кнопку "Создать" попапа добавления карточки
-const formAddCardSubmit = popupAddCard.querySelector('.popup__form');
+const formAddCardSubmit = popupAddCard.querySelector(selectors.form);
 
 // // ищем форму попапа добавления карточки
 // const formAddCard = popupAddCard.querySelector('.');
 // ищем контейнер, в который будем вставлять карточки
-const cardsList = document.querySelector('.elements__list');
+const cardsList = document.querySelector(selectors.cardsList);
 // ищем шаблон карточки
-const cardTemplate = document.querySelector('.element-tmp').content;
+const cardTemplate = document.querySelector(selectors.cardTemplate).content;
 // ищем инпут названия карточки
-let cardNameInput = formAddCardSubmit.querySelector('.form__input_type_name');
+let cardNameInput = formAddCardSubmit.querySelector(selectors.inputImgName);
 // ищем инпут ссылки на картинку карточки
-let cardLinkInput = formAddCardSubmit.querySelector('.form__input_type_link');
+let cardLinkInput = formAddCardSubmit.querySelector(selectors.inputImgLink);
+// ищем попап просмотра фото
+const popupExpandPic = document.querySelector(selectors.popupExpandPic);
+// ищем кнопку закрытия просмотра фото
+const popupCloseExpandPicButton = popupExpandPic.querySelector(selectors.buttonClose);
+
 
 
 // функция добавления карточки
 const addCard = (nameValue, imgValue) => {
   // ищем элемент карточки, который создавать
-  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  const cardElement = cardTemplate.querySelector(selectors.card).cloneNode(true);
   // ищем картинку
-  const cardImg = cardElement.querySelector('.element__image');
+  const cardImg = cardElement.querySelector(selectors.cardImg);
   // ищем название картинки
-  const cardName = cardElement.querySelector('.element__title');
+  const cardName = cardElement.querySelector(selectors.cardName);
   // ищем кнопку лайка карточки
-  const cardLikeButton = cardElement.querySelector('.element__like-button');
+  const cardLikeButton = cardElement.querySelector(selectors.buttonLike);
   // ищем кнопку удаления карточки
-  const cardDeleteButton = cardElement.querySelector('.element__del-button');
+  const cardDeleteButton = cardElement.querySelector(selectors.buttonDel);
+  // ищем фото попапа просмотра
+  const expandPicImage = popupExpandPic.querySelector(selectors.fullSizeImg);
+  // ищем название фото попапа просмотра
+  const expandPicName = popupExpandPic.querySelector(selectors.fullSizeImgCaption);
 
-  //задаем соответствия аргументам функции
+  // задаем соответствия аргументам функции
   cardName.textContent = nameValue;
   cardImg.src = imgValue;
   cardImg.alt = nameValue;
-  // вставляем элемент в начало списка
+  // вставляем элемент перед концом
   cardsList.prepend(cardElement);
+
+  /*---------------------------------------------------------------------------------------*/
+  //Full Size (работа с попапом)
+  // функция открытия попапа просмотра фото
+  const openExpandPicPopup = () => {
+    togglePopup(popupExpandPic);
+    // задаем начальные атрибуты (ссылка, название, alt) для попапа просмотра изображения
+    expandPicImage.src = cardImg.src;
+    expandPicName.textContent = cardName.textContent;
+    expandPicImage.alt = cardName.textContent;
+  };
+
+  // функция закрывания попапа просмотра фото
+  const closeExpandPicPopup = () => {
+    popupExpandPic.classList.remove('popup_opened');
+  };
+
+  // слушаем нажатие на картинку карточки
+  cardImg.addEventListener('click', openExpandPicPopup);
+  // слушаем кнопку закрытия попапа просмотра фото
+  popupCloseExpandPicButton.addEventListener('click', closeExpandPicPopup);
 
   //Likes
   // функция добавления класса (модификатора) кнопке лайк
@@ -121,52 +124,56 @@ const addCard = (nameValue, imgValue) => {
   };
   // слушаем кнопку корзины
   cardDeleteButton.addEventListener('click', cardRemove);
-
+//конец функции addCard
 };
 // функция загрузки карточек из массива
-const addCardsFromArray = function (array) {
-  array.map(function(card) {
+const addCardsFromArray = (array) => {
+  array.map((card) => {
     return addCard(card.name, card.link);
   });
 };
 // вызываем функцию добавления карточек из массива с аргументом нашего массива
 addCardsFromArray(initialCards);
 
-//Edit-Profile (работа с попапом)
+/*---------------------------------------------------------------------------------------*/
 // функция закрытия попапа
-const closePopup = function (close) {
+const closePopup = (close) => {
   close.classList.remove('popup_opened');
 }
+
 // функция открытия/закрытия попапа
 const togglePopup = (popup) => {
   popup.classList.toggle('popup_opened');
 };
 
-
-// функция закрытия попапа по клику вне окна
-const closePopupByClickOnOverlay = function (e) {
+// функция закрытия попапа по клику вне окна попапа
+const closePopupByClickOnOverlay = (e) => {
   if (e.target !== e.currentTarget) {
     return;
   }
   closePopup(popupEditProfile);
   closePopup(popupAddCard);
-}
+  closePopup(popupExpandPic);
+};
+// слушатели событий по клику вне попапов
 popupEditProfile.addEventListener('click', closePopupByClickOnOverlay);
 popupAddCard.addEventListener('click', closePopupByClickOnOverlay);
+popupExpandPic.addEventListener('click', closePopupByClickOnOverlay);
 
-
+/*---------------------------------------------------------------------------------------*/
+//Edit-Profile (работа с попапом)
 // функция, выполняемая по нажатию кнопки редактирования профиля
-const openEditProfilePopup = function () {
+const openEditProfilePopup = () => {
   togglePopup(popupEditProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 // функция закрывания попапа редактирования профиля
-const closeEditProfilePopup = function () {
+const closeEditProfilePopup = () => {
   togglePopup(popupEditProfile);
 };
 // функция отправки данных редактирования профиля
-const editFormSubmitHandler = function (evt) {
+const editFormSubmitHandler = (evt) => {
   evt.preventDefault(); //отменяет дефолтную отправку данных
   evt.stopPropagation(); //отменяет всплытие события
   profileName.textContent = nameInput.value;
@@ -183,15 +190,15 @@ formEditProfileSubmit.addEventListener('submit', editFormSubmitHandler);
 /*---------------------------------------------------------------------------------------*/
 //Add-Card (работа с попапом)
 // функция, выполняемая по нажатию кнопки добавления карточки
-const openAddCardPopup = function () {
+const openAddCardPopup = () => {
   togglePopup(popupAddCard);
 }
 // функция закрывания попапа добавления карточки
-const closeAddCardPopup = function () {
+const closeAddCardPopup = () => {
   togglePopup(popupAddCard);
 };
 // функция отправки данных добавленой карточки
-const addFormSubmitHandler = function (evt) {
+const addFormSubmitHandler = (evt) => {
   evt.preventDefault(); //отменяет дефолтную отправку данных
   evt.stopPropagation(); //отменяет всплытие события
   const name = cardNameInput;
