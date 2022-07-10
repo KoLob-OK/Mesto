cd341092ed135a41b6783107db8ddc6f15ee183f
Konstantin <loboda-nn@yandex.ru>
2022-07-10 12:37:56 +0300

    bugfixAfterReview

--- scripts/index.js
+++ scripts/index.js
@@ -32,11 +32,11 @@ const popupCloseButton = popupEditProfile.querySelector(selectors.buttonClose);
 // ищем форму попапа редактирования профиля
 const formEditProfileSubmit = popupEditProfile.querySelector(selectors.form);
 // ищем инпуты (переменные) формы попапа редактирования профиля
-let nameInput = popupEditProfile.querySelector(selectors.inputUsername);
-let jobInput = popupEditProfile.querySelector(selectors.inputJob);
+const nameInput = popupEditProfile.querySelector(selectors.inputUsername);
+const jobInput = popupEditProfile.querySelector(selectors.inputJob);
 // задаем переменные, в которые будут сохраняться данные из формы
-let profileName = document.querySelector(selectors.userName);
-let profileJob = document.querySelector(selectors.userJob);
+const profileName = document.querySelector(selectors.userName);
+const profileJob = document.querySelector(selectors.userJob);
 
 // ищем попап добавления карточки
 const popupAddCard = document.querySelector(selectors.popupAddCard);
@@ -47,103 +47,99 @@ const popupCloseAddCardButton = popupAddCard.querySelector(selectors.buttonClose
 // ищем кнопку "Создать" попапа добавления карточки
 const formAddCardSubmit = popupAddCard.querySelector(selectors.form);
 
-// // ищем форму попапа добавления карточки
-// const formAddCard = popupAddCard.querySelector('.');
 // ищем контейнер, в который будем вставлять карточки
 const cardsList = document.querySelector(selectors.cardsList);
 // ищем шаблон карточки
 const cardTemplate = document.querySelector(selectors.cardTemplate).content;
-// ищем инпут названия карточки
-let cardNameInput = formAddCardSubmit.querySelector(selectors.inputImgName);
-// ищем инпут ссылки на картинку карточки
-let cardLinkInput = formAddCardSubmit.querySelector(selectors.inputImgLink);
+// ищем инпуты (переменные) формы попапа добавления карточки
+const cardNameInput = formAddCardSubmit.querySelector(selectors.inputImgName);
+const cardLinkInput = formAddCardSubmit.querySelector(selectors.inputImgLink);
+
 // ищем попап просмотра фото
 const popupExpandPic = document.querySelector(selectors.popupExpandPic);
 // ищем кнопку закрытия просмотра фото
 const popupCloseExpandPicButton = popupExpandPic.querySelector(selectors.buttonClose);
+// ищем фото попапа просмотра
+const expandPicImage = popupExpandPic.querySelector(selectors.fullSizeImg);
+// ищем название фото попапа просмотра
+const expandPicName = popupExpandPic.querySelector(selectors.fullSizeImgCaption);
+
+//Likes
+// функция добавления класса (модификатора) кнопке лайк
+const toggleLike = (e) => {
+  e.target.classList.toggle('element__like-button_active');
+};
+
+//Delete
+// функция удаления карточки
+const cardRemove = (e) => {
+  e.target.closest(selectors.card).remove();
+};
+
+//Full Size (работа с попапом)
+// функция открытия попапа просмотра фото
+const openExpandPicPopup = (e) => {
+  // задаем начальные атрибуты (ссылка, название, alt) для попапа просмотра изображения
+  expandPicImage.src = e.target.src;
+  expandPicName.textContent = e.target.alt;
+  expandPicImage.alt = e.target.alt;
+  openPopup(popupExpandPic);
+};
+// слушаем кнопку закрытия попапа просмотра фото
+popupCloseExpandPicButton.addEventListener('click', () => closePopup(popupExpandPic));
 
 
-
-// функция добавления карточки
-const addCard = (nameValue, imgValue) => {
+const createCard = (nameValue, imgValue) => {
   // ищем элемент карточки, который создавать
   const cardElement = cardTemplate.querySelector(selectors.card).cloneNode(true);
+
   // ищем картинку
   const cardImg = cardElement.querySelector(selectors.cardImg);
-  // ищем название картинки
-  const cardName = cardElement.querySelector(selectors.cardName);
-  // ищем кнопку лайка карточки
-  const cardLikeButton = cardElement.querySelector(selectors.buttonLike);
-  // ищем кнопку удаления карточки
-  const cardDeleteButton = cardElement.querySelector(selectors.buttonDel);
-  // ищем фото попапа просмотра
-  const expandPicImage = popupExpandPic.querySelector(selectors.fullSizeImg);
-  // ищем название фото попапа просмотра
-  const expandPicName = popupExpandPic.querySelector(selectors.fullSizeImgCaption);
-
-  // задаем соответствия аргументам функции
-  cardName.textContent = nameValue;
+  // задаем соответствия аргументам
   cardImg.src = imgValue;
   cardImg.alt = nameValue;
-  // вставляем элемент перед концом
-  cardsList.prepend(cardElement);
-
-  /*---------------------------------------------------------------------------------------*/
-  //Full Size (работа с попапом)
-  // функция открытия попапа просмотра фото
-  const openExpandPicPopup = () => {
-    togglePopup(popupExpandPic);
-    // задаем начальные атрибуты (ссылка, название, alt) для попапа просмотра изображения
-    expandPicImage.src = cardImg.src;
-    expandPicName.textContent = cardName.textContent;
-    expandPicImage.alt = cardName.textContent;
-  };
-
-  // функция закрывания попапа просмотра фото
-  const closeExpandPicPopup = () => {
-    popupExpandPic.classList.remove('popup_opened');
-  };
-
   // слушаем нажатие на картинку карточки
   cardImg.addEventListener('click', openExpandPicPopup);
-  // слушаем кнопку закрытия попапа просмотра фото
-  popupCloseExpandPicButton.addEventListener('click', closeExpandPicPopup);
 
-  //Likes
-  // функция добавления класса (модификатора) кнопке лайк
-  const toggleLike = () => {
-    cardLikeButton.classList.toggle('element__like-button_active');
-  };
+  // ищем название картинки
+  const cardName = cardElement.querySelector(selectors.cardName);
+  cardName.textContent = nameValue;
+
+  // ищем кнопку лайка карточки
+  const cardLikeButton = cardElement.querySelector(selectors.buttonLike);
   // слушаем кнопку лайк
   cardLikeButton.addEventListener('click', toggleLike);
 
-  //Delete
-  // функция удаления карточки
-  const cardRemove = () => {
-    cardElement.remove();
-  };
+  // ищем кнопку удаления карточки
+  const cardDeleteButton = cardElement.querySelector(selectors.buttonDel);
   // слушаем кнопку корзины
   cardDeleteButton.addEventListener('click', cardRemove);
-//конец функции addCard
-};
-// функция загрузки карточек из массива
+
+  return cardElement;
+}
+
 const addCardsFromArray = (array) => {
-  array.map((card) => {
-    return addCard(card.name, card.link);
+  const newList = [];
+  array.reverse().forEach((card) => {
+    const newCard = createCard(card.name, card.link);
+    newList.push(newCard);
   });
+  // вставляем элемент перед концом с разбиванием массива оператором spread на аргументы функции
+  cardsList.prepend(...newList);
 };
+
 // вызываем функцию добавления карточек из массива с аргументом нашего массива
 addCardsFromArray(initialCards);
 
 /*---------------------------------------------------------------------------------------*/
 // функция закрытия попапа
-const closePopup = (close) => {
-  close.classList.remove('popup_opened');
+const closePopup = (popup) => {
+  popup.classList.remove('popup_opened');
 }
 
 // функция открытия/закрытия попапа
-const togglePopup = (popup) => {
-  popup.classList.toggle('popup_opened');
+const openPopup = (popup) => {
+  popup.classList.add('popup_opened');
 };
 
 // функция закрытия попапа по клику вне окна попапа
@@ -151,9 +147,7 @@ const closePopupByClickOnOverlay = (e) => {
   if (e.target !== e.currentTarget) {
     return;
   }
-  closePopup(popupEditProfile);
-  closePopup(popupAddCard);
-  closePopup(popupExpandPic);
+  closePopup(e.currentTarget);
 };
 // слушатели событий по клику вне попапов
 popupEditProfile.addEventListener('click', closePopupByClickOnOverlay);
@@ -164,55 +158,50 @@ popupExpandPic.addEventListener('click', closePopupByClickOnOverlay);
 //Edit-Profile (работа с попапом)
 // функция, выполняемая по нажатию кнопки редактирования профиля
 const openEditProfilePopup = () => {
-  togglePopup(popupEditProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  openPopup(popupEditProfile);
 }
-// функция закрывания попапа редактирования профиля
-const closeEditProfilePopup = () => {
-  togglePopup(popupEditProfile);
-};
 // функция отправки данных редактирования профиля
-const editFormSubmitHandler = (evt) => {
+const handleEditFormSubmit = (evt) => {
   evt.preventDefault(); //отменяет дефолтную отправку данных
   evt.stopPropagation(); //отменяет всплытие события
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglePopup(popupEditProfile);
+  closePopup(popupEditProfile);
 }
 // слушаем кнопку редактирования профиля пользователя
 popupEditProfileButton.addEventListener('click', openEditProfilePopup);
 // слушаем кнопку закрытия попапа редактирования профиля пользователя
-popupCloseButton.addEventListener('click', closeEditProfilePopup);
+popupCloseButton.addEventListener('click', () => closePopup(popupEditProfile));
 // Submit формы данных пользователя
-formEditProfileSubmit.addEventListener('submit', editFormSubmitHandler);
+formEditProfileSubmit.addEventListener('submit', handleEditFormSubmit);
 
 /*---------------------------------------------------------------------------------------*/
 //Add-Card (работа с попапом)
 // функция, выполняемая по нажатию кнопки добавления карточки
 const openAddCardPopup = () => {
-  togglePopup(popupAddCard);
+  openPopup(popupAddCard);
 }
-// функция закрывания попапа добавления карточки
-const closeAddCardPopup = () => {
-  togglePopup(popupAddCard);
-};
+
 // функция отправки данных добавленой карточки
-const addFormSubmitHandler = (evt) => {
+const handleAddFormSubmit = (evt) => {
   evt.preventDefault(); //отменяет дефолтную отправку данных
   evt.stopPropagation(); //отменяет всплытие события
   const name = cardNameInput;
-  const img = cardLinkInput;
-  // вызываем функцию addCard с параметрами значений
-  addCard(name.value, img.value);
+  const link = cardLinkInput;
+  const addNewCardValue = createCard(name.value, link.value);
+  // добавляем в начало галереи
+  cardsList.prepend(addNewCardValue);
+  closePopup(popupAddCard);
   // обнуляем поля формы
-  name.value = "";
-  img.value = "";
-  togglePopup(popupAddCard);
+  formAddCardSubmit.reset();
 }
 // слушаем кнопку добавления карточки
 popupAddCardButton.addEventListener('click', openAddCardPopup);
 // слушаем кнопку закрытия попапа добавления карточки
-popupCloseAddCardButton.addEventListener('click', closeAddCardPopup);
+popupCloseAddCardButton.addEventListener('click', () => closePopup(popupAddCard));
 // Submit формы карточки
-formAddCardSubmit.addEventListener('submit', addFormSubmitHandler);
+formAddCardSubmit.addEventListener('submit', handleAddFormSubmit);
+
+
